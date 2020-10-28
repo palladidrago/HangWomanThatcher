ee39a697078b419bb4ca976236b4a030db70b9bd
Max Power <palladidrago@gmail.com>
2020-10-28 16:35:53 +0200

    Push the project

---
+++ HangWoman Thatcher/Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace HangWoman_Thatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labels = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.keyboard = new System.Windows.Forms.GroupBox();
+            this.okeButton = new System.Windows.Forms.Button();
+            this.delButton = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.roniMan = new System.Windows.Forms.PictureBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.labels.SuspendLayout();
+            this.keyboard.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.roniMan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labels
+            // 
+            this.labels.Controls.Add(this.label9);
+            this.labels.Controls.Add(this.label8);
+            this.labels.Controls.Add(this.label7);
+            this.labels.Controls.Add(this.label6);
+            this.labels.Controls.Add(this.label5);
+            this.labels.Controls.Add(this.label3);
+            this.labels.Controls.Add(this.label4);
+            this.labels.Controls.Add(this.label2);
+            this.labels.Controls.Add(this.label1);
+            this.labels.Location = new System.Drawing.Point(98, 36);
+            this.labels.Name = "labels";
+            this.labels.Size = new System.Drawing.Size(562, 134);
+            this.labels.TabIndex = 1;
+            this.labels.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label6.Location = new System.Drawing.Point(260, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 39);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "_";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label5.Location = new System.Drawing.Point(219, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 39);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "_";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label3.Location = new System.Drawing.Point(135, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 39);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "_";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label4.Location = new System.Drawing.Point(177, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 39);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "_";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(93, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 39);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "_";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(51, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "_";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(31, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 81);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Letter_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(124, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 81);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Letter_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(217, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 81);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Letter_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(310, 46);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 81);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Letter_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(403, 46);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 81);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "E";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_Letter_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(496, 46);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(87, 81);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "F";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_Letter_Click);
+            // 
+            // keyboard
+            // 
+            this.keyboard.Controls.Add(this.okeButton);
+            this.keyboard.Controls.Add(this.delButton);
+            this.keyboard.Controls.Add(this.button7);
+            this.keyboard.Controls.Add(this.button8);
+            this.keyboard.Controls.Add(this.button9);
+            this.keyboard.Controls.Add(this.button1);
+            this.keyboard.Controls.Add(this.button6);
+            this.keyboard.Controls.Add(this.button2);
+            this.keyboard.Controls.Add(this.button5);
+            this.keyboard.Controls.Add(this.button3);
+            this.keyboard.Controls.Add(this.button4);
+            this.keyboard.Location = new System.Drawing.Point(12, 227);
+            this.keyboard.Name = "keyboard";
+            this.keyboard.Size = new System.Drawing.Size(758, 229);
+            this.keyboard.TabIndex = 3;
+            this.keyboard.TabStop = false;
+            this.keyboard.Text = "Enter the letters";
+            // 
+            // okeButton
+            // 
+            this.okeButton.Enabled = false;
+            this.okeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.okeButton.Location = new System.Drawing.Point(637, 85);
+            this.okeButton.Name = "okeButton";
+            this.okeButton.Size = new System.Drawing.Size(75, 42);
+            this.okeButton.TabIndex = 3;
+            this.okeButton.Text = "OKE";
+            this.okeButton.UseVisualStyleBackColor = true;
+            this.okeButton.Click += new System.EventHandler(this.buttonOke);
+            // 
+            // delButton
+            // 
+            this.delButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.delButton.Location = new System.Drawing.Point(637, 46);
+            this.delButton.Name = "delButton";
+            this.delButton.Size = new System.Drawing.Size(75, 42);
+            this.delButton.TabIndex = 3;
+            this.delButton.Text = "DELET";
+            this.delButton.UseVisualStyleBackColor = true;
+            this.delButton.Click += new System.EventHandler(this.buttonDel);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // roniMan
+            // 
+            this.roniMan.Image = global::HangWoman_Thatcher.Properties.Resources.p0;
+            this.roniMan.Location = new System.Drawing.Point(804, 0);
+            this.roniMan.Name = "roniMan";
+            this.roniMan.Size = new System.Drawing.Size(313, 409);
+            this.roniMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.roniMan.TabIndex = 4;
+            this.roniMan.TabStop = false;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(182, 133);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(87, 81);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "G";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_Letter_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(275, 133);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(87, 81);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "H";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_Letter_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(368, 133);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(87, 81);
+            this.button9.TabIndex = 2;
+            this.button9.Text = "I";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_Letter_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label7.Location = new System.Drawing.Point(302, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 39);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "_";
+            this.label7.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label8.Location = new System.Drawing.Point(344, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 39);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "_";
+            this.label8.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label9.Location = new System.Drawing.Point(386, 57);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 39);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "_";
+            this.label9.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1189, 531);
+            this.Controls.Add(this.roniMan);
+            this.Controls.Add(this.keyboard);
+            this.Controls.Add(this.labels);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.labels.ResumeLayout(false);
+            this.labels.PerformLayout();
+            this.keyboard.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.roniMan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox labels;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox keyboard;
+        private System.Windows.Forms.Button okeButton;
+        private System.Windows.Forms.Button delButton;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox roniMan;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ HangWoman Thatcher/Form1.cs
@@ -0,0 +1,146 @@
+﻿using HangWoman_Thatcher.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HangWoman_Thatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Image[] tempImg={ Resources.p1, Resources.p2, Resources.p3, Resources.p4, Resources.p5, Resources.p6, };
+            m_Images = tempImg;
+        }
+        int letterAmount=9;
+
+        private void setButtons(bool want)
+        {
+            //Enables/Disables all the buttons.
+            int butCount = 1;
+            Button curButton;
+            for (char c = 'A'; c < letterAmount + 'A'; c++)
+            {
+                curButton = keyboard.Controls["button" + butCount] as Button;
+                curButton.Text = c.ToString();
+                curButton.Enabled = want;
+                butCount++;
+            }
+        }
+        private void Restart()
+        {
+            //Restart the game
+            roniMan.Image = Resources.p0;
+            
+            for (int i = 1; i <= labels.Controls.Count; i++)
+            {
+                labels.Controls["label" + i].Text = "_";
+            }
+            setButtons(true);
+            m_isFirst = true;
+            m_CurrentLabel = 1; m_CountError = 0; m_RightCount = 0;
+            m_WordToGuess = "";
+            delButton.Visible = true;
+            okeButton.Visible = true;
+        }
+
+        int m_CurrentLabel = 1, m_CountError = 0,m_RightCount=0;
+        bool m_isFirst = true;
+        Image[] m_Images;
+        string m_WordToGuess;
+       
+
+        private void button_Letter_Click(object sender, EventArgs e)
+        {
+            //Handles keyboard button clicks.
+            Button btn = sender as Button;
+            string buttonText = btn.Text;
+            if (m_isFirst) {
+                //If first player, let input letters.
+                labels.Controls["label" + m_CurrentLabel].Text = buttonText;
+                m_CurrentLabel++;
+                if (m_CurrentLabel == letterAmount+1)
+                {
+                    okeButton.Enabled = true;
+                    setButtons(false);
+                }
+            }
+            else
+            {
+                //Else, if second player, check if letter pressed is in word, if so put it in the labels and increment
+                //the right count, else, add to picture and increment the wrong count.
+
+                if (m_WordToGuess.Contains(buttonText))
+                {
+                    for (int i = 0; i < m_WordToGuess.Length; i++)
+                        if (m_WordToGuess[i].ToString() == buttonText)
+                        {
+                            labels.Controls["label" + (i + 1)].Text = buttonText;
+                            m_RightCount++;
+                        }
+                }
+                else
+                {
+
+                    //Todo=add losing
+                    m_CountError++;
+
+                    roniMan.Image = m_Images[m_CountError ];
+
+
+                }
+                //Disable the pressed button.
+                (sender as Button).Enabled = false;
+                //If won, restart.
+                if (m_RightCount==letterAmount) { MessageBox.Show("YOU WON BRO !!!!!!!!!!!!!!");Restart(); }
+                if (m_CountError == 5) { MessageBox.Show("YOU LOST BRO :("); Restart(); }
+
+            }
+        }
+
+        private void buttonDel(object sender, EventArgs e)
+        {
+            if (m_CurrentLabel > 1)
+            {
+                labels.Controls["label" + (m_CurrentLabel - 1)].Text = "_";
+                m_CurrentLabel--;
+                okeButton.Enabled = false;
+                setButtons(true);
+            }
+
+        }
+        
+        private void buttonOke(object sender, EventArgs e)
+        {
+            Label curLabel;
+            for (int i = 1; i <= letterAmount; i++)
+            {
+                curLabel = labels.Controls["label" + i] as Label;
+                m_WordToGuess += curLabel.Text;
+                curLabel.Text = "_";
+            }
+            setButtons(true);
+            delButton.Visible = false;
+            okeButton.Visible = false;
+            m_isFirst = false;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
